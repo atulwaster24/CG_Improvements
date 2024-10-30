54621b2da5f45cacd9552e8fadef45e728304093
Atul <atulwasterstar@gmail.com>
2024-10-30 11:48:02 +0530

    Configured git actions for CI/CD automation

--- config/scripts/deploy.sh
+++ config/scripts/deploy.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-# Change to your project directory
-cd /CG_Improvements
+# Define variables
+REPO_URL="https://github.com/atulwaster24/CG_Improvements"  # Update with your repository's URL
+PROJECT_DIR="/CG_Improvements"  # Update with the project directory path
+APP_NAME="CG_Improvements"  # PM2 app name
 
-# Pull the latest changes
-git pull origin main
+# Check if the project directory exists
+if [ -d "$PROJECT_DIR" ]; then
+    echo "Directory exists. Pulling latest changes..."
+    cd $PROJECT_DIR
+    git pull origin main
+else
+    echo "Directory does not exist. Cloning repository..."
+    git clone $REPO_URL $PROJECT_DIR
+    cd $PROJECT_DIR
+fi
 
 # Install dependencies
 npm install
@@ -12,5 +22,6 @@ npm install
 # Build the Next.js project
 npm run build
 
-# Restart the application with PM2
-pm2 restart CG_Improvements
+# Restart the app with PM2
+pm2 start npm --name "$APP_NAME" -- start --watch || pm2 restart "$APP_NAME" --watch
+
