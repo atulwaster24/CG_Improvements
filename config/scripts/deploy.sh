286c534a9e69235a366bb5774238dcc349503245
Atul <atulwasterstar@gmail.com>
2024-11-01 12:26:40 +0530

    Configured git actions for CI/CD automation

--- config/scripts/deploy.sh
+++ config/scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define variables
-REPO_URL="https://github.com/atulwaster24/CG_Improvements"  # Update with your repository's URL
-PROJECT_DIR="/CG_Improvements"  # Update with the project directory path
+REPO_URL="https://github.com/atulwaster24/CG_Improvements"
+PROJECT_DIR="$HOME/CG_Improvements"  # Use an absolute path in the home directory
 APP_NAME="CG_Improvements"  # PM2 app name
 
 # Check if the project directory exists
@@ -22,6 +22,9 @@ npm install
 # Build the Next.js project
 npm run build
 
-# Restart the app with PM2
+# Start or restart the app with PM2
 pm2 start npm --name "$APP_NAME" -- start --watch || pm2 restart "$APP_NAME" --watch
 
+# Ensure PM2 starts on reboot
+pm2 startup -u $USER --hp $HOME
+pm2 save
