33640766d47ec0ad53821b9aa622ac0b67bbf014
Atul <atulwasterstar@gmail.com>
2024-11-04 12:13:24 +0530

    Updated Deploy.sh File

--- config/scripts/deploy.sh
+++ config/scripts/deploy.sh
@@ -22,8 +22,14 @@ npm install
 # Build the Next.js project
 npm run build
 
-# Start or restart the app with PM2
-pm2 start npm --name "$APP_NAME" -- start --watch || pm2 restart "$APP_NAME" --watch
+# Check if the PM2 process with the name $APP_NAME exists
+if pm2 describe "$APP_NAME" > /dev/null; then
+    # If the process exists, restart it
+    pm2 restart "$APP_NAME" --watch
+else
+    # If the process doesn't exist, start it
+    pm2 start npm --name "$APP_NAME" -- start --watch
+fi
 
 # Ensure PM2 starts on reboot
 pm2 startup -u $USER --hp $HOME
