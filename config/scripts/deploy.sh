cdf0573c61eb0c140cafa6700b532fd66f3538dd
Atul <atulwasterstar@gmail.com>
2024-11-04 12:47:48 +0530

    Updated Deploy.sh File

--- config/scripts/deploy.sh
+++ config/scripts/deploy.sh
@@ -5,17 +5,6 @@ REPO_URL="https://github.com/atulwaster24/CG_Improvements"
 PROJECT_DIR="$HOME/CG_Improvements"  # Use an absolute path in the home directory
 APP_NAME="CG_Improvements"  # PM2 app name
 
-# Check if the project directory exists
-if [ -d "$PROJECT_DIR" ]; then
-    echo "Directory exists. Pulling latest changes..."
-    cd $PROJECT_DIR
-    git pull origin main
-else
-    echo "Directory does not exist. Cloning repository..."
-    git clone $REPO_URL $PROJECT_DIR
-    cd $PROJECT_DIR
-fi
-
 # Install dependencies
 npm install
 
